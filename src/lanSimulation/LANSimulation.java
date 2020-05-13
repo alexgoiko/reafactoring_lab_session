4a4201ff58636aeeb2d9c4fb29a3108e646fae5b
dmh1001 <dmh1001@alu.ubu.es>
2020-05-13 19:57:51 +0200

    metodo printSimulated

--- src/lanSimulation/LANSimulation.java
+++ src/lanSimulation/LANSimulation.java
@@ -25,11 +25,11 @@ import java.io.*;
 
 public class LANSimulation {
 
-	public static void doRegressionTests () {
-		junit.textui.TestRunner.run (LANTests.suite());
+	public static void doRegressionTests() {
+		junit.textui.TestRunner.run(LANTests.suite());
 	}
 
-	public static void simulate () {
+	public static void simulate() {
 		Network network = Network.DefaultExample();
 		StringWriter report = new StringWriter(100);
 		StringBuffer buf = new StringBuffer(100);
@@ -51,85 +51,78 @@ public class LANSimulation {
 
 		System.out.println("---------------------------------SCENARIOS------------------------------------------");
 		String document = "author: FILIP   Hello World";
-		System.out.print("'Filip' prints '" + document + "' on 'Andy': ");
-		System.out.print(network.requestWorkstationPrintsDocument("Filip", document, "Andy", report));
-		System.out.println(" (expects true);");
+		printSimulated(network, report, document, "Andy", true);
 
-		System.out.print("'Filip' prints '" + document + "' on 'UnknownPrinter': ");
-		System.out.print(network.requestWorkstationPrintsDocument("Filip", document, "UnknownPrinter", report));
-		System.out.println(" (expects false);");
+		printSimulated(network, report, document, "UnknownPrinter", false);
 
-		System.out.print("'Filip' prints '" + document + "' on 'Hans': ");
-		System.out.print(network.requestWorkstationPrintsDocument("Filip", document, "Hans", report));
-		System.out.println(" (expects false);");
+		printSimulated(network, report, document, "Hans", false);
 
-		System.out.print("'Filip' prints '" + document + "' on 'n1': ");
-		System.out.print(network.requestWorkstationPrintsDocument("Filip", document, "n1", report));
-		System.out.println(" (expects false);");
+		printSimulated(network, report, document, "n1", false);
 
 		document = "Hello World";
-		System.out.print("'Filip' prints '" + document + "' on 'Andy': ");
-		System.out.print(network.requestWorkstationPrintsDocument("Filip", document, "Andy", report));
-		System.out.println(" (expects true);");
+		printSimulated(network, report, document, "Andy", true);
 
 		document = "!PS Hello World in postscript.author:Filip.title:Hello.";
-		System.out.print("'Filip' prints '" + document + "' on 'Andy': ");
-		System.out.print(network.requestWorkstationPrintsDocument("Filip", document, "Andy", report));
-		System.out.println(" (expects true);");
+		printSimulated(network, report, document, "Andy", true);
 
 		System.out.print("'Filip' prints '" + document + "' on 'Hans': ");
 		System.out.print(network.requestWorkstationPrintsDocument("Filip", document, "Hans", report));
 		System.out.println(" (expects false);");
 
 		document = "!PS Hello World in postscript.Author:Filip.Title:Hello.";
-		System.out.print("'Filip' prints '" + document + "' on 'Andy': ");
-		System.out.print(network.requestWorkstationPrintsDocument("Filip", document, "Andy", report));
-		System.out.println(" (expects true);");
+		printSimulated(network, report, document, "Andy", true);
 
 		document = "!PS Hello World in postscript.author:Filip;title:Hello;";
-		System.out.print("'Filip' prints '" + document + "' on 'Andy': ");
-		System.out.print(network.requestWorkstationPrintsDocument("Filip", document, "Andy", report));
-		System.out.println(" (expects true);");
+		printSimulated(network, report, document, "Andy", true);
 
 		document = "!PS Hello World in postscript.author:.title:.";
-		System.out.print("'Filip' prints '" + document + "' on 'Andy': ");
-		System.out.print(network.requestWorkstationPrintsDocument("Filip", document, "Andy", report));
-		System.out.println(" (expects true);");
+		printSimulated(network, report, document, "Andy", true);
 
 		try {
 			System.out.print("'UnknownWorkstation' prints 'does not matter' on 'does not matter': ");
-			System.out.print(network.requestWorkstationPrintsDocument("UnknownWorkstation",
-					"does not matter", "does not matter", report));
+			System.out.print(network.requestWorkstationPrintsDocument("UnknownWorkstation", "does not matter",
+					"does not matter", report));
 			System.out.println(" (??? no exception);");
 		} catch (AssertionError e1) {
 			System.out.println("exception (as expected);");
-		};
+		}
+		;
 
 		System.out.print("BROADCAST REQUEST: ");
 		System.out.print(network.requestBroadcast(report));
 		System.out.println(" (expects true);");
 
-		System.out.println(); System.out.println(); System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
 		System.out.println("---------------------------------REPORT------------------------------------------");
 		System.out.println(report.toString());
 	}
 
-	public static void main (String args[]) {
+	private static void printSimulated(Network network, StringWriter report, String document, String nombre,
+			Boolean resultadoEsperado) {
+		System.out.print("'Filip' prints '" + document + "' on '" + nombre + "' : ");
+		System.out.print(network.requestWorkstationPrintsDocument("Filip", document, nombre, report));
+		System.out.println(" (expects " + resultadoEsperado + ");");
+	}
+
+	public static void main(String args[]) {
 
 		if (args.length <= 0) {
-			System.out.println("Usage: t(est) | s(imulate) nrOfIterations '");	    
-		} else if (args[0].equals("t")) {//'test' command
+			System.out.println("Usage: t(est) | s(imulate) nrOfIterations '");
+		} else if (args[0].equals("t")) {// 'test' command
 			doRegressionTests();
-		} else if (args[0].equals("s")) {//'simulate' command
+		} else if (args[0].equals("s")) {// 'simulate' command
 			Integer nrOfIters = new Integer(1);
 			if (args.length > 1) {
 				nrOfIters = new Integer(args[1]);
-			};
+			}
+			;
 
 			for (int i = 0; i < nrOfIters.intValue(); i++) {
 				simulate();
 			}
-		} else {//unknown commaND
+		} else {// unknown commaND
 			System.out.print("Unknown command to LANSimulation: '");
 			System.out.print(args[0]);
 			System.out.println("'");
